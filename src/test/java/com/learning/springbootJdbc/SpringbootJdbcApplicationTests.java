d0683e0ab4e70c59a72e3879e156c016776c02b0
Manikanta Yarramsetti <manikantayarramsetti445@gmail.com>
2024-04-25 17:49:42 +0530

    jdbc project

---
+++ src/test/java/com/learning/springbootJdbc/SpringbootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootJdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
